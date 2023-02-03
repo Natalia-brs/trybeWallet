3ba55c55d7f20615fd85db6ce99b7f33225ce99e
natalia <ntla.brs@gmail.com>
2023-02-03 17:11:03 -0300

    Requisito 7

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
+  renderTable = (dado) => {
+    const { description, method, currency, tag, value, id, exchangeRates } = dado;
+
+    return (
+      <tr key={ id }>
+        <td>
+          {' '}
+          {description}
+          {' '}
+        </td>
+        <td>
+          {' '}
+          { tag }
+          {' '}
+        </td>
+        <td>
+          {' '}
+          { method }
+          {' '}
+        </td>
+        <td>
+          {' '}
+          { Number(value).toFixed(2) }
+          {' '}
+        </td>
+        <td>
+          {' '}
+          { exchangeRates[currency].name }
+          {' '}
+        </td>
+        <td>
+          {' '}
+          { parseFloat(exchangeRates[currency].ask).toFixed(2) }
+          {' '}
+        </td>
+        <td>
+          {' '}
+          { parseFloat(exchangeRates[currency].ask) * Number(value)}
+        </td>
+        <td> Real </td>
+      </tr>
+    );
+  };
+
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -17,9 +64,20 @@ class Table extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          { expenses && expenses.map((item) => this.renderTable(item)) }
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
